06dcd5f13b6b501dbfeeea63035e0200b3240f8f
Hemant Patel <83568724+Hemant959@users.noreply.github.com>
2023-10-26 16:49:18 +0530

    Delete ExpenseTracker directory

--- ExpenseTracker/src/main/java/com/Hemant/User_Expense/Repo/IAuthenticationTokenRepo.java
+++
@@ -1,8 +0,0 @@
-package com.Hemant.User_Expense.Repo;
-
-import com.Hemant.User_Expense.model.AuthenticationToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IAuthenticationTokenRepo extends JpaRepository<AuthenticationToken,Long> {
-
-}
--- ExpenseTracker/src/main/java/com/Hemant/User_Expense/Repo/IPTokenRepo.java
+++
@@ -1,7 +0,0 @@
-package com.Hemant.User_Expense.Repo;
-
-import com.Hemant.User_Expense.model.AuthenticationToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IPTokenRepo extends JpaRepository<AuthenticationToken,Long> {
-}
